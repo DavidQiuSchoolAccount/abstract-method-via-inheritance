c54499ab4da354e7a14c9fae1f1de3b0469fc62c
Matt <mchan00@stuy.edu>
2019-03-02 01:29:10 -0500

    Fixed randomization

--- Retriever.java
+++ Retriever.java
@@ -3,7 +3,7 @@ import java.security.SecureRandom;
 public class Retriever extends Pet {
 	public String speak() {
 		SecureRandom random = new SecureRandom();
-		int randomInt = random.next(101);
+		int randomInt = random.nextInt(101);
 		if (randomInt < 50){
 			return "arf";
 		}
@@ -12,6 +12,6 @@ public class Retriever extends Pet {
 		}
 
 		// Secret surprise (if lucky)
-		return "I am not a dog, but a sentient human and cannot be told to speak merely one repetetive ononomatopoeia."
+		return "I am not a dog, but a sentient human and cannot be told to speak merely one repetetive ononomatopoeia.";
 	}
 }
