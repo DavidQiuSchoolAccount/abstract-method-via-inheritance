dd3a34f9d292e58f049b27580c08d569f642ca84
Matt <mchan00@stuy.edu>
2019-03-02 00:10:18 -0500

    Added random options for the speech of Retriever and formatted the print message

--- Retriever.java
+++ Retriever.java
@@ -1,5 +1,17 @@
+import java.security.SecureRandom;
+
 public class Retriever extends Pet {
 	public String speak() {
-		return "arf!";
+		SecureRandom random = new SecureRandom();
+		int randomInt = random.next(101);
+		if (randomInt < 50){
+			return "arf";
+		}
+		if (randomInt < 100){
+			return "woof";
+		}
+
+		// Secret surprise (if lucky)
+		return "I am not a dog, but a sentient human and cannot be told to speak merely one repetetive ononomatopoeia."
 	}
 }
